9f4320abe6743062c4252d523d95cd8c782a1bc9
lord semi <josemi.masip@gmail.com>
2018-06-01 12:12:09 +0200

    WIP: Controller player Cenital

---
+++ ThisIsYou/Assets/PlayerControllerCenital.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(PlayerModel))]
+public class PlayerControllerCenital : MonoBehaviour
+{
+
+
+    private PlayerModelCenital _playerModelCenital;
+
+    void Awake()
+    {
+
+        _playerModelCenital = GetComponent<PlayerModelCenital>();
+    }
+
+
+    void Update()
+    {
+
+        float x = 0, y = 0;//float por si hubiera mando o para normalizar
+
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            y = -1;
+        }
+        else if (Input.GetKey(KeyCode.UpArrow))
+        {
+            y = 1;
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            x = -1;
+        }
+        else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            x = 1;
+        }
+
+        x = Input.GetAxis("Horizontal");
+        y = Input.GetAxis("Vertical");
+
+        _playerModelCenital.setPos(x, y);
+    }
+}
---
+++ ThisIsYou/Assets/PlayerModelCenital.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerModelCenital : MonoBehaviour
+{
+
+    //Hide in inspector (o no, da un poco igual)
+    float _x = 0, _y = 0;
+
+    public int _facingDirection = 1;
+    public Vector2 _snapArea = new Vector2(2.5f, 2.5f);
+
+    private Rigidbody2D _rigidbody;
+    private ParticleSystem _particleSystem;
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.white;
+        Gizmos.DrawWireCube(transform.position, new Vector3(_snapArea.x, _snapArea.y, 0));
+    }
+
+    void Awake()
+    {
+        _rigidbody = GetComponent<Rigidbody2D>();
+        _particleSystem = GetComponent<ParticleSystem>();
+
+        //_groundedActionState = new GroundedActionState(this);
+        //_airborneActionState = new AirborneActionState(this);
+    }
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        if (_rigidbody.velocity.x != 0)
+        {
+            _facingDirection = (int)(_rigidbody.velocity.x / Mathf.Abs(_rigidbody.velocity.x));
+        }
+        move(_x, _y);
+
+    }
+    void move(float _x, float _y)
+    {
+
+        _rigidbody.velocity = new Vector2(_x * Time.deltaTime, _y * Time.deltaTime);
+
+    }
+
+    public void setPos(float x, float y)
+    {
+        _x = x;
+        _y = y;
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        //Recibir daño
+    }
+}
