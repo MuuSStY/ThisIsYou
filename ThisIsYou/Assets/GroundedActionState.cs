8313f060781c4786974a8993dae6a07808fc1132
IDEC-UPF\mcv16_alumne <daniel.tomas@alum.upf.edu>
2018-06-01 11:03:54 +0200

    wip

--- ThisIsYou/Assets/GroundedActionState.cs
+++ ThisIsYou/Assets/GroundedActionState.cs
@@ -11,8 +11,6 @@ public class GroundedActionState : ActionState {
     float _highJumpGroundVelocityThreshold = 0f;
     float _yHighJumpVelocity1 = 0f;
     float _yHighJumpVelocity0 = 0f;
-    float _xLongJumpVelocity = 0f;
-    float _yLongJumpVelocity = 0f;
 
     public GroundedActionState(PlayerModel player) : base(player)
     {
@@ -25,31 +23,11 @@ public class GroundedActionState : ActionState {
     public override void OnStateEnter(ActionState lastState)
     {
         base.OnStateEnter(lastState);
-        _player._isLongJump = false;
-        GetPowerStateValues();
     }
 
     public override void OnStateExit(ActionState nextState)
     {
         base.OnStateExit(nextState);
-    }
-
-    public override void RefreshPowerState()
-    {
-        GetPowerStateValues();
-    }
-
-    protected void GetPowerStateValues()
-    {
-        _groundSkiddingDeceleration = _player._currentPowerState.GetGroundSkiddingDeceleration();
-        _groundReleaseDeceleration = _player._currentPowerState.GetGroundRealeaseDeceleration();
-        _xMaxGroundSpeed = _player._currentPowerState.GetXMaxGroundSpeed();
-        _xGroundAcceleration = _player._currentPowerState.GetXGroundAcceleration();
-        _highJumpGroundVelocityThreshold = _player._currentPowerState.GetHighJumpGroundVelocityThreshold();
-        _yHighJumpVelocity1 = _player._currentPowerState.GetYHighJumpVelocity1();
-        _yHighJumpVelocity0 = _player._currentPowerState.GetYHighJumpVelocity0();
-        _xLongJumpVelocity = _player._currentPowerState.GetXLongJumpVelocity();
-        _yLongJumpVelocity = _player._currentPowerState.GetYLongJumpVelocity();
     }
 
     public override void MovementInput(float x, float y)
@@ -89,12 +67,5 @@ public class GroundedActionState : ActionState {
             _rigidbody.velocity += Vector2.up * _yHighJumpVelocity0;
         }
         //Si dejamos el cambio de estado para ground sensor podemos hacer una transición a jump squat antes de saltar, también sirve por si hay algún obstáculo que te impida saltar
-    }
-
-    public override void OnJumpLongButton()
-    {
-        base.OnJumpLongButton();
-        _rigidbody.velocity = new Vector2(_player._facingDirection * _xLongJumpVelocity, _yLongJumpVelocity);
-        _player._isLongJump = true;//el problema aquí es si no llegara a despegar del suelo, de momento lo hago así
     }
 }
