23016f343ed4235a4c8c1b30938f2b290054671c
James <jamesphelandeharo@gmail.com>
2018-06-01 13:00:32 +0200

    This is you

--- ThisIsYou/Assets/Scripts/CanvasIntroController.cs
+++ ThisIsYou/Assets/Scripts/CanvasIntroController.cs
@@ -7,6 +7,8 @@ public class CanvasIntroController : MonoBehaviour {
     public Camera _camera;
     public Text _text;
     public Image _image;
+
+    private bool _opacity_done;
     private float _size_to_reach;
 	// Use this for initialization
 	void Start () {
@@ -14,6 +16,7 @@ public class CanvasIntroController : MonoBehaviour {
         Color c = _text.color;
         c.a = 0.0f;
         _text.color = c;
+        _opacity_done = false;
     }
 	
 	// Update is called once per frame
@@ -21,13 +24,25 @@ public class CanvasIntroController : MonoBehaviour {
         float size = _camera.orthographicSize;
 
         if (size == _size_to_reach)
-        {
-            Color c = _text.color;
-            Color c_image = _image.color;
-            c.a = 1.0f;
-            _text.color = c;
-            c_image.a = 0.0f;
-            _image.color = c_image;
+        {        
+            if (!_opacity_done)
+            {
+                Color c = _text.color;
+                Color c_image = _image.color;
+
+                if (c.a == 1.0f)
+                {
+                    _opacity_done = true;
+                }
+                else
+                {
+                    c.a += 0.01f;
+                    c_image.a -= 0.01f;
+
+                    _text.color = c;
+                    _image.color = c_image;
+                }
+            }
         }
 	}
 }
