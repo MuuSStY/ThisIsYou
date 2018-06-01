a57737457dd754b029ba8e0ed7a1bfd472b71736
lord semi <josemi.masip@gmail.com>
2018-06-01 19:48:36 +0200

    Triggers added

---
+++ ThisIsYou/Assets/Scripts/OnTriggerEvent.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OnTriggerEvent : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            Slider slider = GameObject.Find("Slider2").GetComponent<Slider>();
+            slider.value = 0.7f;
+            Debug.Log(slider.value);
+        }
+    }
+}
---
+++ ThisIsYou/Assets/Scripts/onTriggerEvent2.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class onTriggerEvent2 : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            Slider slider = GameObject.Find("Slider2").GetComponent<Slider>();
+            slider.value = 0.3f;
+            Debug.Log(slider.value);
+        }
+    }
+}
