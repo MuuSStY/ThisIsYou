564973980dbc6fe0683375b2214633c1c2288f7e
IDEC-UPF\mcv16_alumne <daniel.tomas@alum.upf.edu>
2018-06-01 19:27:38 +0200

    GameManager y PaintTimer

---
+++ ThisIsYou/Assets/Scripts/PaintTimer.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaintTimer : MonoBehaviour
+{
+
+    GameManager manager;
+    public float timeToResults = 5f;
+    public float timeToLoadNextScene = 2f;
+
+    void Awake()
+    {
+        manager = FindObjectOfType<GameManager>();
+    }
+
+    void Start()
+    {
+        StartCoroutine(CountTimeForResults(timeToResults));
+        Debug.Log("start time");
+    }
+
+    void Update()
+    {
+    }
+
+    IEnumerator CountTimeForResults(float time)
+    {
+        yield return new WaitForSeconds(time);
+        Debug.Log("time's up");
+        StartCoroutine(CountTimeForSceneChange(timeToLoadNextScene));
+    }
+
+    IEnumerator CountTimeForSceneChange(float time)
+    {
+        yield return new WaitForSeconds(time);
+        manager.LoadScene(GameManager.ScenesToLoad.INTRO);
+    }
+
+}
