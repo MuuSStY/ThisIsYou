3ee849dc738c8e64e25c628405fc9b60da772c4d
James <jamesphelandeharo@gmail.com>
2018-06-01 12:46:57 +0200

    This is you opacity 0

--- ThisIsYou/Assets/Scripts/CanvasIntroController.cs
+++ ThisIsYou/Assets/Scripts/CanvasIntroController.cs
@@ -1,16 +1,28 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CanvasIntroController : MonoBehaviour {
     public Camera _camera;
+    public Text _text;
+    public Image _image;
+    private float _size_to_reach;
 	// Use this for initialization
 	void Start () {
-		
-	}
+        _size_to_reach = _camera.GetComponent<CameraController>()._currentTargetSize;
+        Color c = _text.color;
+        c.a = 0.0f;
+        _text.color = c;
+    }
 	
 	// Update is called once per frame
 	void Update () {
-		
+        float size = _camera.orthographicSize;
+
+        if (size == _size_to_reach)
+        {
+
+        }
 	}
 }
