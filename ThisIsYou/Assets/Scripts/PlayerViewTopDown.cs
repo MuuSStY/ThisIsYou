48a14d8de51290040e1b4085ade11c5e2d93a23f
IDEC-UPF\mcv16_alumne <daniel.tomas@alum.upf.edu>
2018-06-01 13:52:05 +0200

    wip animations player

---
+++ ThisIsYou/Assets/Scripts/PlayerViewTopDown.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Animator))]
+//[RequireComponent(typeof(PlayerModelTopDown))]
+public class PlayerViewTopDown : MonoBehaviour {
+
+    private Animator animator;
+    private PlayerModel playerModel; 
+	
+	void Start () {
+        animator = GetComponent<Animator>();
+        playerModel = GetComponent<PlayerModel>();
+	}
+	
+    
+	void Update () {
+        UpdateFacingDirection();
+	}
+
+    void UpdateFacingDirection()
+    {
+        //Vector3 direction = playerModel.GetFacingDirection();
+        animator.SetFloat("DirectionX", 1);
+        animator.SetFloat("DirectionY", 1);
+
+        //if (direction != Vector3.zero)
+        //{
+        //    animator.SetFloat("DirectionX", direction.x);
+        //    animator.SetFloat("DirectionY", direction.y);
+        //}
+
+        //animator.SetBool("isMoving", playerModelTopDown.IsMoving());
+    }
+}
