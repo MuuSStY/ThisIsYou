e2c5c36c82b4d36531f3f01afa11b9bfd347546e
Daniel Tomas <ticidici@yahoo.es>
2018-06-02 01:51:39 +0200

    cámara sigue chachi

--- ThisIsYou/Assets/Scripts/CameraController.cs
+++ ThisIsYou/Assets/Scripts/CameraController.cs
@@ -5,19 +5,21 @@ using UnityEngine;
 public class CameraController : MonoBehaviour {
 
     private Camera _camera;
-
-    public float _currentTargetSize;
+    private Transform playerTransform;
+    public float _currentTargetSize = 5.0f;
 
     public float _zoomVelocity = 0.4f;
 
     void Awake ()
     {
         _camera = GetComponent<Camera>();
-        _currentTargetSize = 5.0f;
+        playerTransform = FindObjectOfType<PlayerModel>().GetComponent<Transform>();
     }
 	
 	void Update ()
     {
+        ChangeCameraOffset();
+
         float size = _camera.orthographicSize;
 
         if (_currentTargetSize - size < 0)
@@ -34,6 +36,12 @@ public class CameraController : MonoBehaviour {
             _camera.orthographicSize = _currentTargetSize;
         }
 	}
+
+    void ChangeCameraOffset(){
+        float playerHeight = Mathf.Clamp(playerTransform.position.y, 0, 5.5f);
+        float ratio = 1 - playerHeight / 5.5f;
+        transform.localPosition =  new Vector3(0, Mathf.Lerp(-1.5f, 4f, ratio), -10);
+    }
 
     public void ChangeCameraTargetSize(int powerLevel)
     {
