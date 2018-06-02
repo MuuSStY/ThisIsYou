514075729d4b564242fa51f8d24c5f91335acf67
IDEC-UPF\mcv16_alumne <daniel.tomas@alum.upf.edu>
2018-06-02 12:00:00 +0200

    slider chachi

---
+++ ThisIsYou/Assets/Scripts/SexualitySlider.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SexualitySlider : MonoBehaviour {
+    public static SexualitySlider instance;
+
+    Slider slider;
+
+	void Awake () {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(this.gameObject);
+        }
+
+        DontDestroyOnLoad(this);
+
+        slider = GetComponent<Slider>();
+	}
+
+    void Start()
+    {
+        slider.value = 0.5f;
+    }
+
+    void Update () {
+		
+	}
+
+    public void AddToBar(float fraction)
+    {
+        slider.value += fraction;
+    }
+
+    public float GetValue()
+    {
+        return slider.value;
+    }
+}
